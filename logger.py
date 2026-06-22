5ec35e5138361df98910fab496a895c748c20337
binazirmahnia <binazir.mahnia@gmail.com>
2026-06-22 14:26:17 +0330

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,4 +1,7 @@
 import logging
 
 def setup_logger():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s"
+    )
