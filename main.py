37712fa79a47e169c3d383ddb341f7bfb14b399c
binazirmahnia <binazir.mahnia@gmail.com>
2026-06-22 14:20:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from bot.handlers import build_application
-from utils.logger import setup_logger
+from bot_handlers import build_application
 from config import TELEGRAM_BOT_TOKEN
+from logger import setup_logger
 import sys
 
 setup_logger()
 
 def main():
     if not TELEGRAM_BOT_TOKEN:
-        print("❌ Missing TELEGRAM_BOT_TOKEN")
+        print("Missing TELEGRAM_BOT_TOKEN")
         sys.exit(1)
 
     app = build_application()
-    print("🚀 Vizaris is LIVE...")
-    app.run_polling()
+    print("🚀 Vizaris PRO SaaS running...")
+    app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
